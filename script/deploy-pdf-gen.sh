a03f74b23c2ebc325ea392e61078d22e3861a744
Sabin <sadams@eventsquid.com>
2020-05-20 23:31:59 -0700

    ksdjflasdj

--- script/deploy-pdf-gen.sh
+++ script/deploy-pdf-gen.sh
@@ -4,6 +4,7 @@ ssh jenkins@23.239.4.176 mkdir /home/www/pdf-server-tmp
 scp -r * jenkins@23.239.4.176:/home/www/pdf-server-tmp
 
 ssh jenkins@23.239.4.176 << EOF
+    export PATH=/usr/local/bin
     cd /home/www/pdf-server-tmp
     npm i
     cd ..
